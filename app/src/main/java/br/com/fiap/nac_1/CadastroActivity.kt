af34508caab8de08be4799da261b154824eb76e4
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-05 21:49:26 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/nac_1/CadastroActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.nac_1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
